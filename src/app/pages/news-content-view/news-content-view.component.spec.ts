eae9fbdc1ed0fcb01267fc100f5a9afd4e3489c3
akaravi <mac.karavi@ntk.ir>
2021-03-01 09:54:21 +0330

    update angular ver

--- src/app/pages/news-content-view/news-content-view.component.spec.ts
+++ src/app/pages/news-content-view/news-content-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NewsContentViewComponent } from './news-content-view.component';
 
@@ -6,7 +6,7 @@ describe('NewsContentViewComponent', () => {
   let component: NewsContentViewComponent;
   let fixture: ComponentFixture<NewsContentViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NewsContentViewComponent ]
     })
